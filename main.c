6f22a25985ac7882df0dabcec0ce00fbfaf05b32
bottleba <bottleba@student.21-school.ru>
2024-10-12 00:52:44 +0300

    Project_PIN_encrypt

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "pin_encrypt.h"
+
+int main (){
+    
+    // input from keyboard
+    // a, b, c, d
+    enctipt(1, 2, 3, 4);
+    
+    return 0;
+}
---
+++ pin_encrypt.h
@@ -0,0 +1,9 @@
+#ifndef PIN_ENCRYPT_H
+#define PIN_ENCRYPT_H
+
+typedef unsigned char ubyte;
+typedef unsigned int uint_32;
+ 
+uint_32 encrypt (ubyte, ubyte, ubyte, ubyte);
+
+#endif // PIN_ENCRYPT_H
